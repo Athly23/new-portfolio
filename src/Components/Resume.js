7454e74c72dba38ed87e495ca8db98603113040f
Athly23 <andrewly06@gmail.com>
2021-03-17 11:24:25 -0400

    testing 4

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,8 +6,8 @@ class Resume extends Component {
     return (
       <div>
         <a
-          href="https://docs.google.com/document/d/13cIELRE-HFa1btAVgWrzqvd21zn58tGjkDYwH_2foq0/edit?usp=sharing"
-          rel="noopener noreferrer"
+          href="https://docs.google.com/document/d/13cIELRE-HFa1btAVgWrzqvd21zn58tGjkDYwH_2foq0/edit?usp=sharing" //new resume link to updated resume
+          rel="noopener noreferrer" //brings to new page
           target="_blank"
         ></a>
         {/* <img src={pdf}></img> */}
